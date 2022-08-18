1517ae4a204767ade9f5dd4a948d4c82e04ca688
Amit Roy <amit.roy@g.bracu.ac.bd>
2022-08-19 01:25:19 +0600

    project update

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,6 @@
+<div style="clear:both" class="panel panel-default">
+    <div class="panel-body">
+      Amit Fashion Gallery
+    </div>
+    <div class="panel-footer">Customer Care : +8801785212798</div>
+  </div>
